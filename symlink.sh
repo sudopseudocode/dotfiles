3f916d0304cde244bd4ae42e48886d628ba1035f
Paul DiLoreto <paul.diloreto@gmail.com>
2019-05-27 16:32:19 -0700

    feat(root): Updated symlink script to include cloning ZSH plugins

--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,12 @@
 
 # For now, I'm assuming this repo is in my specific Dropbox filepath
 FILEPATH=${HOME}/Dropbox/dotfiles
+
+# Revert oh-my-zsh and oh-my-tmux to master branch
 git submodule update -f
+# Add custom plugins to oh-my-zsh directory 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Without the -n it adds a symlinked directory with the same name inside its own directory
 ln -s -f -n $FILEPATH/oh-my-zsh ${HOME}/.oh-my-zsh
