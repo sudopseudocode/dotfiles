3bdaf58618ae8cb128ac255df3c76205077d7749
Paul DiLoreto <paul.diloreto@gmail.com>
2019-11-04 18:45:50 -0800

    feat(root): Added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+# Completely removes vim files (you may not want this)
+rm -rf ~/.vim
+rm -rf ~/.config/nvim
+
+# Remove other dotfiles
+rm -rf ~/.oh-my-zsh
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.tmux.conf.local
+rm ~/.tmux.conf
