a82df062619cbc2ed297dd4bba0998e0fdbc0376
Paul DiLoreto <paul.diloreto@gmail.com>
2019-11-17 12:55:34 -0800

    feat(root): Switched to pulseaudio for better control of devices

--- swaybar.sh
+++ swaybar.sh
@@ -44,15 +44,15 @@ fi
 
 # Audio
 ###############################
-audio_volume=$(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')
-audio_is_muted=$(amixer sget Master | awk -F"[][]" '/dB/ { print $6 }')
-if [ $audio_is_muted = "off" ]
+audio_volume=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{print $1}'` --get-volume)
+audio_is_muted=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{print $1}'` --get-mute)
+if [ $audio_is_muted = "true" ]
 then
     audio_active='🔇'
 else
     audio_active='🔊'
 fi
-audio_content="$audio_active  $audio_volume"
+audio_content="$audio_active  $audio_volume%"
 
 # Network
 ###############################
@@ -84,10 +84,5 @@ cpu_content="$cpu_speed   ($cpu_temp)"
 used_space="$(df -BG | grep nvme0n1p4 | awk '{print $3}')"
 available_space="$(df -BG | grep nvme0n1p4 | awk '{print $4}')"
 storage_content="$used_space / $available_space"
-
-# Others
-###############################
-# language=$(swaymsg -r -t get_inputs | awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' | grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' | grep "xkb_active_layout_name" | awk -F '"' '{print $4}')
-# loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 
 echo "$player_content$network_content$separator$storage_content$separator$cpu_content$separator$audio_content$separator$battery_content$separator$date_content  "
