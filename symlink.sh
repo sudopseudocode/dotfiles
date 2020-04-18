01f0405700ae9b7dfcaa707fd704851ec5f85581
Paul DiLoreto <paul.diloreto@gmail.com>
2020-04-18 15:53:47 -0700

    refactor(root): Cleaned up bash script

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# For now, I'm assuming this repo is in my specific Dropbox filepath
+# Prerequisites:
+# - Install zsh
+# - Install tmux
+# - Install neovim
+# - Install fzf
+# - Install ripgrep
+
+# For Mac:
+# - Install yabai
+# - Install skhd
+
+# For Linux:
+# - Install alacritty
+
+# I assume the script is being run from it's own directory
 FILEPATH=$(pwd)
 
 # Revert oh-my-zsh and oh-my-tmux to master branch
@@ -9,39 +23,37 @@ git submodule update --init --recursive
 git clone https://github.com/zsh-users/zsh-autosuggestions $FILEPATH/oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $FILEPATH/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 # Installs tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Installs nnn plugins
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 
 # Without the -n it adds a symlinked directory with the same name inside its own directory
-ln -sfn $FILEPATH/oh-my-zsh ${HOME}/.oh-my-zsh
+ln -sfn $FILEPATH/oh-my-zsh ~/.oh-my-zsh
 
-# TODO add alacritty settings
 
-# For regular Vim
-# ln -sf $FILEPATH/vimrc ${HOME}/.vimrc
-
-# For NeoVim
-ln -sf $FILEPATH/vimrc ${HOME}/.config/nvim/init.vim
-
-ln -sf $FILEPATH/zshrc ${HOME}/.zshrc
-ln -sf $FILEPATH/tmux.conf.local ${HOME}/.tmux.conf.local
-ln -sf $FILEPATH/oh-my-tmux/.tmux.conf ${HOME}/.tmux.conf
-
-# Create nvim directories if they don't exist
-mkdir -p ${HOME}/.vim
-mkdir -p ${HOME}/.config/nvim
+# TODO move these into .config
+ln -sf $FILEPATH/zshrc ~/.zshrc
+ln -sf $FILEPATH/tmux.conf.local ~/.tmux.conf.local
+ln -sf $FILEPATH/oh-my-tmux/.tmux.conf ~/.tmux.conf
 
 # Create UltiSnips in .vim folder
-ln -s -f $FILEPATH/ultisnips ${HOME}/.config/coc/ultisnips
+mkdir -p ~/.config/coc
+ln -s -f $FILEPATH/ultisnips ~/.config/coc/ultisnips
 
 # Finally link Neovim's config
-ln -s -f $FILEPATH/vimrc ${HOME}/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+ln -s -f $FILEPATH/vimrc ~/.config/nvim/init.vim
 
-# Install vim-plug (Unix)
+# Install vim-plug (should work on Mac & Linux)
 # Neovim
 # curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Vim
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Mac-specific settings
+# mkdir -p ~/.config/yabai
+# mkdir -p ~/.config/skhd
+# ln -sf $FILEPATH/yabairc ~/.config/yabai/yabairc
+# ln -sf $FILEPATH/skhdrc ~/.config/skhd/skhdrc
+
+# Linux-specific settings
+# TODO add alacritty
