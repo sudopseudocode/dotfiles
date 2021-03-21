26ce89c1567f4590389496fa58b9d7ba305d880b
Paul DiLoreto <paul.diloreto@gmail.com>
2021-03-21 04:29:34 -0700

    feat(root): Added Alacritty.yml to install script

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -22,8 +22,17 @@
 # After Install:
 # - Run command to install tmux plugins
 
-# I assume the script is being run from it's own directory
-FILEPATH=$(pwd)
+# Get filepath of this script
+FILE_PATH=$(dirname $(dirname $SCRIPT_PATH))
+
+# TODO if MAC
+# TODO install brew
+# brew update && brew upgrade
+# brew install coreutils
+# brew install yabai
+# brew install skhd
+
+# TODO if Linux
 
 # Revert oh-my-zsh and oh-my-tmux to master branch
 git submodule update --init --recursive
@@ -36,20 +45,24 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 
 # Without the -n it adds a symlinked directory with the same name inside its own directory
-ln -sfn $FILEPATH/oh-my-zsh ~/.oh-my-zsh
+ln -sfn $FILEPATH/oh-my-zsh $HOME/.oh-my-zsh
 
 # TODO move these into .config
-ln -sf $FILEPATH/zshrc ~/.zshrc
-ln -sf $FILEPATH/tmux.conf ~/.tmux.conf
+ln -sf $FILEPATH/zshrc $HOME/.zshrc
+ln -sf $FILEPATH/tmux.conf $HOME/.tmux.conf
 
 # Create UltiSnips in .vim folder
 mkdir -p ~/.config/coc
-ln -sfn $FILEPATH/ultisnips ~/.config/coc/ultisnips
-ln -sf $FILEPATH/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sfn $FILEPATH/ultisnips $HOME/.config/coc/ultisnips
+ln -sf $FILEPATH/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
 # Finally link Neovim's config
 mkdir -p ~/.config/nvim
-ln -sf $FILEPATH/vimrc ~/.config/nvim/init.vim
+ln -sf $FILEPATH/vimrc $HOME/.config/nvim/init.vim
+
+# Install Alacritty for both Mac & Linux
+mkdir -p ~/.config/alacritty
+ln -sf $FILEPATH/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 # Install vim-plug (should work on Mac & Linux)
 # Neovim
@@ -63,4 +76,9 @@ ln -sf $FILEPATH/vimrc ~/.config/nvim/init.vim
 # ln -sf $FILEPATH/skhdrc ~/.config/skhd/skhdrc
 
 # Linux-specific settings
-# TODO add alacritty
+# Add config folders
+# mkdir -p ${HOME}/.config/sway
+
+# Add symlinks
+# ln -sf $(pwd)/swayconfig ${HOME}/.config/sway/config
+# ln -sf $(pwd)/swaybar.sh ${HOME}/.config/sway/swaybar.sh
--- install-scripts/linuxinstall.sh
+++
@@ -1,6 +0,0 @@
-# Add config folders
-mkdir -p ${HOME}/.config/sway
-
-# Add symlinks
-ln -sf $(pwd)/swayconfig ${HOME}/.config/sway/config
-ln -sf $(pwd)/swaybar.sh ${HOME}/.config/sway/swaybar.sh
