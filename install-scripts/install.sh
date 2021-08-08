7ba385d4cbe2abaf6b5a50da57bb62b991e83348
Paul DiLoreto <paul.diloreto@gmail.com>
2021-08-07 23:34:06 -0700

    feat(root): Use ~ and realpath in install script

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -4,15 +4,13 @@
 # - Open Neovim and run command to install Tmux plugins
 # - Add Nord theme to Slack
 
-# Get filepath of this script
-FILE_PATH=$(dirname $(dirname $SCRIPT_PATH))
-
 # TODO if MAC
 # TODO install brew
 # brew update && brew upgrade
 # brew install coreutils \
 #   alacritty \
 #   starship \
+#   trash \
 #   zsh \
 #   tmux \
 #   neovim \
@@ -54,57 +52,63 @@ FILE_PATH=$(dirname $(dirname $SCRIPT_PATH))
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Add custom plugins to oh-my-zsh directory
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone https://github.com/b4b4r07/zsh-vimode-visual ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vimode-visual
 git clone https://github.com/nyquase/vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/vi-mode
 # Installs tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Installs nnn plugins
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 
 # TODO move these into .config
-rm $HOME/.zshrc
-ln -s $FILEPATH/zshrc $HOME/.zshrc
-rm $HOME/.tmux.conf
-ln -s $FILEPATH/tmux.conf $HOME/.tmux.conf
+rm ~/.zshrc
+ln -s $(realpath ./zshrc) ~/.zshrc
+rm ~/.tmux.conf
+ln -s $(realpath ./tmux.conf) ~/.tmux.conf
 
 # Create UltiSnips in .vim folder
-rm -rf $HOME/.config/coc
-mkdir -p $HOME/.config/coc
-ln -sfn $FILEPATH/ultisnips $HOME/.config/coc/ultisnips
-ln -sf $FILEPATH/coc-settings.json $HOME/.config/nvim/coc-settings.json
+rm -rf ~/.config/coc
+mkdir -p ~/.config/coc
+ln -sfn $(realpath ./ultisnips) ~/.config/coc/ultisnips
+ln -sf $(realpath ./coc-settings.json) ~/.config/nvim/coc-settings.json
 
 # Symlink Neovim's config
-mkdir -p $HOME/.config/nvim
-ln -s $FILEPATH/vimrc $HOME/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+ln -s $(realpath ./vimrc) ~/.config/nvim/init.vim
 
 # Symlink Alacritty folder for both Mac & Linux
-rm -rf $HOME/.config/alacritty
-ln -sn $FILEPATH/alacritty $HOME/.config/alacritty
+rm -rf ~/.config/alacritty
+mkdir -p ~/.config/alacritty
+ln -s $(realpath ./alacritty.yml) ~/.config/alacritty/alacritty.yml
+
+# Symlink lf config
+rm -rf ~/.config/lf
+mkdir -p ~/.config/lf
+ln -s $(realpath ./lfrc) ~/.config/lf/lfrc
 
 # Symlink Starship configuration
-rm -r $HOME/.config/starship.toml
-ln -s $FILEPATH/starship.toml $HOME/.config/starship/toml
+rm -r ~/.config/starship.toml
+ln -s $(realpath ./starship.toml) ~/.config/starship/toml
 
 # Install vim-plug (should work on Mac & Linux)
 # Neovim
-# curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
+# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #### Mac-specific settings
 
-# rm -rf $HOME/.config/yabai
-# mkdir -p $HOME/.config/yabai
-# rm -rf $HOME/.config/skhd
-# mkdir -p $HOME/.config/skhd
-# ln -sf $FILEPATH/yabairc $HOME/.config/yabai/yabairc
-# ln -sf $FILEPATH/skhdrc $HOME/.config/skhd/skhdrc
+# rm -rf ~/.config/yabai
+# mkdir -p ~/.config/yabai
+# rm -rf ~/.config/skhd
+# mkdir -p ~/.config/skhd
+# ln -sf $(realpath ./yabairc) ~/.config/yabai/yabairc
+# ln -sf $(realpath ./skhdrc) ~/.config/skhd/skhdrc
 
 #### Linux-specific settings
 
 # Symlinks for Sway
-# rm -rf $HOME/.config/sway
-# mkdir -p $HOME/.config/sway
+# rm -rf ~/.config/sway
+# mkdir -p ~/.config/sway
 # ln -sf $(pwd)/swayconfig ${HOME}/.config/sway/config
 # ln -sf $(pwd)/swaybar.sh ${HOME}/.config/sway/swaybar.sh
