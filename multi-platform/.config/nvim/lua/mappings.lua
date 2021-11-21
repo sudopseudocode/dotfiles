fc19d4b28a86db9b38f383484c4c5602f92ac78f
Paul DiLoreto <paul.diloreto@gmail.com>
2021-11-21 05:30:16 -0800

    feat(nvim): Added LSP key bindings

--- multi-platform/.config/nvim/lua/mappings.lua
+++ multi-platform/.config/nvim/lua/mappings.lua
@@ -5,8 +5,23 @@ local default_opts = { noremap = true, silent = true }
 g.mapleader = " "
 
 -- LSP bindings
-map('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
-map('n', 'gi', '<Plug>(coc-definition)', { silent = true })
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', default_opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', default_opts)
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', default_opts)
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', default_opts)
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', default_opts)
+map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', default_opts)
+map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', default_opts)
+map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', default_opts)
+map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', default_opts)
+map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', default_opts)
+map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', default_opts)
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', default_opts)
+map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', default_opts)
+map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', default_opts)
+map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', default_opts)
+map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', default_opts)
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', default_opts)
 
 -- Directional Hotkeys
 map('', 'h', '<Nop>', default_opts)
