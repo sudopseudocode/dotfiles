1c3bc6cc108d56975523b36a12ba89e6c580fea8
Paul DiLoreto <paul.diloreto@gmail.com>
2021-11-23 12:28:24 -0800

    feat(nvim): Adding more null-ls settings

--- multi-platform/.config/nvim/lua/configs/lsp/tsserver.lua
+++ multi-platform/.config/nvim/lua/configs/lsp/tsserver.lua
@@ -1,19 +1,31 @@
+local ts_utils = require("nvim-lsp-ts-utils")
+local ts_utils_settings = {
+    -- debug = true,
+    enable_import_on_completion = true,
+    import_all_scan_buffers = 100,
+    update_imports_on_move = true,
+    -- filter out dumb module warning
+    filter_out_diagnostics_by_code = { 80001 },
+}
+local common_on_attach = require('configs.lsp.on_attach')
+
 return {
+    init_options = ts_utils.init_options,
     on_attach = function(client, bufnr)
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
-        local ts_utils = require("nvim-lsp-ts-utils")
-        ts_utils.setup({
-            eslint_bin = "eslint_d",
-            eslint_enable_diagnostics = true,
-            eslint_enable_code_actions = true,
-            enable_formatting = true,
-            formatter = "prettier",
-        })
+
+        common_on_attach(client, bufnr)
+
+        ts_utils.setup(ts_utils_settings)
         ts_utils.setup_client(client)
-        -- buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
-        -- buf_map(bufnr, "n", "gi", ":TSLspRenameFile<CR>")
-        -- buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
-        on_attach(client, bufnr)
+
+        -- u.buf_map("n", "gs", ":TSLspOrganize<CR>", nil, bufnr)
+        -- u.buf_map("n", "gI", ":TSLspRenameFile<CR>", nil, bufnr)
+        -- u.buf_map("n", "go", ":TSLspImportAll<CR>", nil, bufnr)
+        -- u.buf_map("i", ".", ".<C-x><C-o>", nil, bufnr)
     end,
+    flags = {
+        debounce_text_changes = 150,
+    },
 }
