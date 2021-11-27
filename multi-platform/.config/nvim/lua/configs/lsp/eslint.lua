7285f57654765804706df35f625735b3d6885910
Paul DiLoreto <paul.diloreto@gmail.com>
2021-11-27 12:15:18 -0800

    refactor(nvim): Clean up

--- multi-platform/.config/nvim/lua/configs/lsp/eslint.lua
+++ multi-platform/.config/nvim/lua/configs/lsp/eslint.lua
@@ -1,10 +1,4 @@
-local buf_map = require("utils").buf_map
-local common_on_attach = require("configs.lsp.on_attach")
-
 return {
-    on_attach = function(client, bufnr)
-        common_on_attach(client, bufnr)
-    end,
     -- default command doesn't work on Work computer because Node 12 is in the $PATH
     cmd = {
         "/usr/local/bin/node",
