36df49bc913e6a3c2517fc68e58353e6f72c249c
Paul DiLoreto <paul.diloreto@gmail.com>
2021-11-30 10:24:50 -0800

    chore(nvim): Turn off virtual text for diagnostics

--- multi-platform/.config/nvim/lua/configs/lsp/init.lua
+++ multi-platform/.config/nvim/lua/configs/lsp/init.lua
@@ -68,6 +68,6 @@ return function()
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         underline = true,
         update_in_insert = false,
-        virtual_text = true,
+        virtual_text = false,
     })
 end
