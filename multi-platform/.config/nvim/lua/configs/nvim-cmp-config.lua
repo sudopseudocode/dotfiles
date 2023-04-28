be93e429cf9a334321524a015e0c4efb468a56c3
Paul DiLoreto <paul.diloreto@gmail.com>
2023-04-28 15:58:30 -0700

    fix(nvim): NeoVim cmdline autocomplete

--- multi-platform/.config/nvim/lua/configs/nvim-cmp-config.lua
+++ multi-platform/.config/nvim/lua/configs/nvim-cmp-config.lua
@@ -7,9 +7,6 @@ return function()
             -- REQUIRED - you must specify a snippet engine
             expand = function(args)
                 vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
             end,
         },
         mapping = {
@@ -28,10 +25,7 @@ return function()
         },
         sources = cmp.config.sources({
             { name = "nvim_lsp" },
-            { name = "vsnip" }, -- For vsnip users.
-            -- { name = 'luasnip' }, -- For luasnip users.
-            -- { name = 'ultisnips' }, -- For ultisnips users.
-            -- { name = 'snippy' }, -- For snippy users.
+            { name = "vsnip" },
         }, {
             { name = "buffer" },
         }),
@@ -45,6 +39,7 @@ return function()
 
     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
         sources = {
             { name = "buffer" },
         },
@@ -52,10 +47,16 @@ return function()
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
             { name = "path" },
         }, {
-            { name = "cmdline" },
+            {
+                name = "cmdline",
+                option = {
+                    ignore_cmds = { "Man", "!" },
+                },
+            },
         }),
     })
 end
