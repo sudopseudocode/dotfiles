7cf498f92c5252ede4d6fd07a4b91b2645f259c0
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-05 21:50:24 -0700

    fix(nvim): nvim hop plugin

--- multi-platform/.config/nvim/lua/configs/hop-config.lua
+++ multi-platform/.config/nvim/lua/configs/hop-config.lua
@@ -1,5 +1,6 @@
 return function()
     -- require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+    require("hop").setup()
     vim.keymap.set("n", "s", "<cmd>HopChar2<CR>")
     vim.keymap.set("n", "gl", "<cmd>HopLineStart<CR>")
 end
