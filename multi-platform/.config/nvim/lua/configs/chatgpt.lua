0ac29de2431840373fd7d3def6b1815ba9fd65ba
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-11 11:38:19 -0700

    chore(nvim): tweaked ChatGPT

--- multi-platform/.config/nvim/lua/configs/chatgpt.lua
+++ multi-platform/.config/nvim/lua/configs/chatgpt.lua
@@ -4,6 +4,9 @@ return function()
         popup_input = {
             submit = "<C-s>",
         },
+        openai_params = {
+            model = "gpt-3.5-turbo",
+        },
         -- chat = {
         --     keymaps = {
         --         yank_last_code = "<C-y>",
@@ -11,7 +14,12 @@ return function()
         -- },
     })
 
-    vim.keymap.set("n", "<leader>fc", ":ChatGPT<CR>", {silent=true})
-    vim.keymap.set("n", "<leader>fe", ":ChatGPTEditWithInstructions<CR>", {silent=true})
+    vim.keymap.set("n", "<leader>fc", ":ChatGPT<CR>", { silent = true })
+    vim.keymap.set(
+        "n",
+        "<leader>fe",
+        ":ChatGPTEditWithInstructions<CR>",
+        { silent = true }
+    )
     vim.keymap.set("n", "<leader>fr", ":ChatGPTRun ")
 end
