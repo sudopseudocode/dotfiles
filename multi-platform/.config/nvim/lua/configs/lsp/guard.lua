7e887e0bd08b4303457ef481d6d0e4b36cbe240c
Paul DiLoreto <paul.diloreto@gmail.com>
2023-10-24 12:18:44 -0700

    chore(nvim): guard & lsp changes

--- multi-platform/.config/nvim/lua/configs/lsp/guard.lua
+++ multi-platform/.config/nvim/lua/configs/lsp/guard.lua
@@ -2,12 +2,15 @@ return function()
     local ft = require("guard.filetype")
 
     ft("lua"):fmt("stylua")
-    ft("typescript,javascript,typescriptreact,javascriptreact")
-        :lint("eslint_d")
-        :fmt("prettier")
+    ft("typescript,javascript,typescriptreact,javascriptreact,svelte"):fmt(
+        "lsp"
+    )
+    ft("json,jsonc"):fmt("prettier")
+    ft("html,graphql,css,less,scss"):fmt("prettier")
+    ft("yaml,markdown,mdx"):fmt("prettier")
 
     require("guard").setup({
         fmt_on_save = true,
-        lsp_as_default_formatter = true,
+        lsp_as_default_formatter = false,
     })
 end
