fcd71b0fc189bea7c4d3c8af3fade14db0e7d046
Paul DiLoreto <paul.diloreto@gmail.com>
2023-10-24 19:02:03 -0700

    feat(nvim): use efm, eslint, and lsp-format

--- multi-platform/.config/nvim/lua/configs/lsp/efm.lua
+++ multi-platform/.config/nvim/lua/configs/lsp/efm.lua
@@ -1,25 +1,50 @@
 local get_setup = require("configs.lsp.default-setup")
 local efm_opts = get_setup()
 
-local javascript = {
-    require("efmls-configs.linters.eslint_d"),
-    require("efmls-configs.formatters.eslint_d"),
-    require("efmls-configs.formatters.prettier_d"),
+local prettier = {
+  formatCanRange = true,
+  formatCommand =
+  "prettier --stdin --stdin-filepath '${INPUT}' ${--range-start=charStart} ${--range-end=charEnd} ${--tab-width=tabSize} ${--use-tabs=!insertSpaces}",
+  formatStdin = true,
+  rootMarkers = {
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+  },
+}
+local eslint = {
+  formatCommand = "eslint_d --fix-to-stdout --stdin-filename '${INPUT}' --stdin",
+  formatStdin = true,
+}
+local stylua = {
+  formatCanRange = true,
+  formatCommand = "stylua --color Never ${--range-start:charStart} ${--range-end:charEnd} -",
+  formatStdin = true,
+  rootMarkers = { "stylua.toml", ".stylua.toml" },
 }
 
-efm_opts.init_options = { documentFormatting = true }
+local languages = {
+  lua = { stylua },
+  typescript = { prettier, eslint },
+  javascript = { prettier, eslint },
+  typescriptreact = { prettier, eslint },
+  javascriptreact = { prettier, eslint },
+  css = { prettier },
+  graphql = { prettier },
+  html = { prettier },
+  markdown = { prettier },
+  json = { prettier },
+  python = { prettier },
+}
+
+efm_opts.filetypes = vim.tbl_keys(languages)
+efm_opts.init_options = {
+  documentFormatting = true,
+  documentRangeFormatting = true,
+}
 efm_opts.settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-        lua = {
-            require("efmls-configs.linters.luacheck"),
-            require("efmls-configs.formatters.stylua"),
-        },
-        typescript = javascript,
-        javascript = javascript,
-        typescriptreact = javascript,
-        javascriptreact = javascript,
-    },
+  rootMarkers = { ".git/" },
+  languages = languages,
 }
 
 return efm_opts
