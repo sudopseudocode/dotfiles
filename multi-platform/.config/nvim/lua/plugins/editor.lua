572974ed0ec6936c3ba84e9c9e33110c514044ad
Paul DiLoreto <paul.diloreto@gmail.com>
2023-11-09 15:39:22 -0800

    chore(nvim): add support for work github links

--- multi-platform/.config/nvim/lua/plugins/editor.lua
+++ multi-platform/.config/nvim/lua/plugins/editor.lua
@@ -42,7 +42,11 @@ return {
     "ruifm/gitlinker.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     config = function()
-      require("gitlinker").setup()
+      require("gitlinker").setup({
+        callbacks = {
+          ["github%..+%.com"] = require("gitlinker.hosts").get_github_type_url,
+        },
+      })
     end,
   },
 
