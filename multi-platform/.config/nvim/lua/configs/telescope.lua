81192409ffec87a20aed9fbababdae683fab0a48
Paul DiLoreto <paul.diloreto@gmail.com>
2024-01-04 12:55:14 -0800

    fix(nvim): telescope config

--- multi-platform/.config/nvim/lua/configs/telescope.lua
+++ multi-platform/.config/nvim/lua/configs/telescope.lua
@@ -1,24 +1,24 @@
-local telescope = require("telescope")
-local telescope_builtin = require("telescope.builtin")
--- We cache the results of "git rev-parse"
--- Process creation is expensive in Windows, so this reduces latency
-local is_inside_work_tree = {}
-
-local project_files = function()
-  local cwd = vim.fn.getcwd()
-  if is_inside_work_tree[cwd] == nil then
-    vim.fn.system("git rev-parse --is-inside-work-tree")
-    is_inside_work_tree[cwd] = vim.v.shell_error == 0
-  end
-
-  if is_inside_work_tree[cwd] then
-    telescope_builtin.git_files()
-  else
-    telescope_builtin.find_files()
-  end
-end
-
 return function()
+  local telescope = require("telescope")
+  local telescope_builtin = require("telescope.builtin")
+  -- We cache the results of "git rev-parse"
+  -- Process creation is expensive in Windows, so this reduces latency
+  local is_inside_work_tree = {}
+
+  local project_files = function()
+    local cwd = vim.fn.getcwd()
+    if is_inside_work_tree[cwd] == nil then
+      vim.fn.system("git rev-parse --is-inside-work-tree")
+      is_inside_work_tree[cwd] = vim.v.shell_error == 0
+    end
+
+    if is_inside_work_tree[cwd] then
+      telescope_builtin.git_files()
+    else
+      telescope_builtin.find_files()
+    end
+  end
+
   vim.keymap.set("n", "<leader>ff", project_files, { silent = true })
   vim.keymap.set(
     "n",
