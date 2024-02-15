4b40f7dd17b681be0c9a620ad923c8ae0aec85f5
Paul DiLoreto <paul.diloreto@gmail.com>
2024-02-15 13:39:47 -0800

    fix(nvim): updated indent-blankline & integrate with tree-sitter + rainbow-delimiters

--- multi-platform/.config/nvim/lua/plugins/ui.lua
+++ multi-platform/.config/nvim/lua/plugins/ui.lua
@@ -1,95 +1,97 @@
 return {
-    -- Color theme
-    {
-        "marko-cerovac/material.nvim",
-        config = function()
-            require("material").setup({
-                contrast = {
-                    terminal = true,
-                    sidebars = false,
-                    floating_windows = false,
-                    cursor_line = true,
-                    non_current_windows = true,
-                },
-                plugins = {
-                    "dashboard",
-                    "gitsigns",
-                    "hop",
-                    "indent-blankline",
-                    "lspsaga",
-                    "nvim-cmp",
-                    "nvim-tree",
-                    "nvim-web-devicons",
-                    "telescope",
-                    "trouble",
-                },
-            })
-            -- Set the theme
-            vim.o.background = "dark"
-            vim.g.material_style = "deep ocean"
-            vim.cmd("colorscheme material")
-        end,
-    },
+  -- Color theme
+  {
+    "marko-cerovac/material.nvim",
+    config = function()
+      require("material").setup({
+        contrast = {
+          terminal = true,
+          sidebars = false,
+          floating_windows = false,
+          cursor_line = true,
+          non_current_windows = true,
+        },
+        plugins = {
+          "dashboard",
+          "gitsigns",
+          "hop",
+          "indent-blankline",
+          "lspsaga",
+          "nvim-cmp",
+          "nvim-tree",
+          "nvim-web-devicons",
+          "telescope",
+          "trouble",
+        },
+      })
+      -- Set the theme
+      vim.o.background = "dark"
+      vim.g.material_style = "deep ocean"
+      vim.cmd("colorscheme material")
+    end,
+  },
 
-    -- Welcome dashboard
-    {
-        "nvimdev/dashboard-nvim",
-        config = require("configs.dashboard"),
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+  -- Welcome dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    config = require("configs.dashboard"),
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 
-    -- Show line indents
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            vim.g.indent_blankline_filetype_exclude = { "dashboard" }
-        end,
+  -- Show line indents
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = require("configs.indent-blankline"),
+    dependencies = {
+      "HiPhish/rainbow-delimiters.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
+  },
 
-    -- Smooth scrolling
-    {
-        "karb94/neoscroll.nvim",
-        config = function()
-            require("neoscroll").setup({
-                hide_cursor = true,
-                respect_scrolloff = true,
-                cursor_scrolls_alone = true,
-                performance_mode = true,
-            })
-        end,
-    },
+  -- Smooth scrolling
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({
+        hide_cursor = true,
+        respect_scrolloff = true,
+        cursor_scrolls_alone = true,
+        performance_mode = true,
+      })
+    end,
+  },
 
-    -- Better buffer tab line
-    {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = require("configs.bufferline-config"),
-    },
+  -- Better buffer tab line
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = require("configs.bufferline-config"),
+  },
 
-    -- Status Line
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup()
-        end,
-    },
+  -- Status Line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup()
+    end,
+  },
 
-    -- Color hex codes
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({
-                "*",
-                css = { css = true },
-            })
-        end,
-    },
+  -- Color hex codes
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        "*",
+        css = { css = true },
+      })
+    end,
+  },
 
-    -- Custom number/folds/gitsigns column
-    {
-        "luukvbaal/statuscol.nvim",
-        config = require("configs.statuscol"),
-    },
+  -- Custom number/folds/gitsigns column
+  {
+    "luukvbaal/statuscol.nvim",
+    config = require("configs.statuscol"),
+  },
 }
