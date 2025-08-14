2c32919cda124cbdeda140ba2c26f7f7a9baf511
Paul DiLoreto <paul.diloreto@gmail.com>
2025-08-14 12:18:36 -0700

    feat(nvim): enable normal mode editing in neotree

--- unix-configs/.config/nvim/lua/plugins/ui/neo-tree.lua
+++ unix-configs/.config/nvim/lua/plugins/ui/neo-tree.lua
@@ -51,6 +51,7 @@ return {
         enable_diagnostics = true,
         open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
         open_files_using_relative_paths = false,
+        enable_normal_mode_for_inputs = true,
         sort_case_insensitive = false, -- used when sorting files and directories in the tree
         sort_function = nil, -- use a custom function for sorting files and directories in the tree
         -- sort_function = function (a,b)
@@ -322,9 +323,7 @@ return {
                     end
                   end,
                 })
-                vim.notify(
-                  "Opening folder in Finder: " .. filepath
-                )
+                vim.notify("Opening folder in Finder: " .. filepath)
               elseif node.type == "file" then
                 vim.fn.jobstart({ cmd, filepath }, {
                   detach = true,
