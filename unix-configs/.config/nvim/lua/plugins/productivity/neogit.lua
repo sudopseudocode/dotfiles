8343fb8efbce6918c8fd6c5651678ea3bad20c3c
Paul DiLoreto <paul.diloreto@gmail.com>
2025-08-14 12:24:54 -0700

    feat(nvim): remove unnecessary neogit deps

--- unix-configs/.config/nvim/lua/plugins/productivity/neogit.lua
+++ unix-configs/.config/nvim/lua/plugins/productivity/neogit.lua
@@ -4,11 +4,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
-
-      -- Only one of these is needed.
       "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-      "echasnovski/mini.pick", -- optional
     },
     config = function()
       local neogit = require("neogit")
