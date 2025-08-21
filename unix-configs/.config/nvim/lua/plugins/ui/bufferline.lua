1e39642655004c501abbd94b789c2a320109fdbf
Paul DiLoreto <paul.diloreto@gmail.com>
2025-08-21 16:35:32 -0700

    fix(nvim): disable theme for bufferline, which breaks on latest

--- unix-configs/.config/nvim/lua/plugins/ui/bufferline.lua
+++ unix-configs/.config/nvim/lua/plugins/ui/bufferline.lua
@@ -2,13 +2,12 @@ return {
   -- Better buffer tab line
   {
     "akinsho/bufferline.nvim",
+    tag = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
     after = "catppuccin",
     config = function()
       require("bufferline").setup({
-        highlights = require(
-          "catppuccin.groups.integrations.bufferline"
-        ).get(),
+        -- highlights = require("catppuccin.groups.integrations.bufferline").get(),
         options = {
           numbers = "buffer_id",
           diagnostics = "nvim_lsp",
